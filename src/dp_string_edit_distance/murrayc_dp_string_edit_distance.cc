80cf933da1c2176fcb21c8915196547c06338e34
Murray Cumming <murrayc@murrayc.com>
2015-11-30 12:31:20 +0100

    Slightly improve choice of path.

--- src/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
+++ src/dp_string_edit_distance/murrayc_dp_string_edit_distance.cc
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <iomanip>
+#include <limits>
 
 #define MURRAYC_DP_DEBUG_OUTPUT = 1;
 
@@ -175,22 +176,51 @@ protected:
 class Cost {
 public:
   using uint = unsigned int;
+  
+  enum class Operation {
+    INVALID,
+    MATCH,
+    INSERT,
+    DELETE
+  };
+
+  using type_path = std::vector<Operation>;
 
   explicit Cost()
   : cost(0)
   {}
 
-  explicit Cost(uint in_cost)
-  : cost(in_cost)
+  Cost(uint in_cost, Operation initial_path)
+  : cost(in_cost),
+    path(1, initial_path)
+  {}
+  
+  Cost(uint in_cost, const type_path& in_path)
+  : cost(in_cost),
+    path(in_path)
   {}
 
   Cost(const Cost& src) = default;
   Cost& operator=(const Cost& src) = default;
-  
+
   Cost(Cost&& src) = default;
   Cost& operator=(Cost&& src) = default;
 
+  static std::string get_operation_as_string(Operation op) {
+    switch (op) {
+      case Operation::MATCH:
+        return "match";
+      case Operation::INSERT:
+        return "insert";
+      case Operation::DELETE:
+        return "delete";
+      default:
+        return "invalid";
+    }
+  }
+
   uint cost;
+  type_path path;
 };
 
 class DpEditDistance
@@ -208,12 +238,12 @@ private:
   type_cost calc_cost(uint i, uint j) const override {
     if (i == 0) {
       //Base case:
-      return Cost(j * indel(' '));
+      return Cost(j * indel(' '), Cost::Operation::INSERT);
     }
 
     if (j == 0) {
       //Base case:
-      return Cost(i * indel(' '));
+      return Cost(i * indel(' '), Cost::Operation::DELETE);
     }
 
     const type_costs& costs_i = costs_.get(0);
@@ -226,10 +256,26 @@ private:
     const uint cost_match = costs_i_minus_1[j - 1].cost + match(char_str_i, char_pattern_j);
     const uint cost_insert = costs_i[j - 1].cost + indel(char_pattern_j);
     const uint cost_delete = costs_i_minus_1[j].cost + indel(char_str_i);
-
+    
     auto min = std::min(cost_match, cost_insert);
     min = std::min(min, cost_delete);
-    return Cost(min);
+
+    //Remember the path, based on what operation produced this minimum cost:
+    Cost::type_path path;
+    if (min == cost_match) {
+        path = costs_i_minus_1[j - 1].path;
+        path.emplace_back(Cost::Operation::MATCH);
+    } else if (min == cost_insert) {
+        path = costs_i[j - 1].path;
+        path.emplace_back(Cost::Operation::INSERT);
+    } else if (min == cost_delete) {
+        path = costs_i_minus_1[j].path;
+        path.emplace_back(Cost::Operation::DELETE);
+    } else {
+      std::cerr << "Unexpected min." << std::endl;
+    }
+
+    return Cost(min, path);
   }
 
   void get_goal_cell(unsigned int& i, unsigned int& j) const override {
@@ -260,10 +306,16 @@ int main() {
   const auto pattern = "thou shalt not";
   
   DpEditDistance dp(str, pattern);
-  const auto distance = dp.calc();
+  const auto result = dp.calc();
   std::cout << "string: " << str << std::endl
     << "pattern: " << pattern << std::endl
-    << "distance: " << distance.cost << std::endl;
+    << "distance: " << result.cost << std::endl;
+
+  std::cout << "Operations: ";
+  for (const auto op : result.path) {
+    std::cout << Cost::get_operation_as_string(op) << ", ";
+  }
+  std::cout << std::endl;
   
   return EXIT_SUCCESS;
 }
--- src/dp_string_substring_matching/murrayc_dp_string_substring_matching.cc
+++ src/dp_string_substring_matching/murrayc_dp_string_substring_matching.cc
@@ -5,6 +5,13 @@
 #include <iomanip>
 #include <limits>
 
+#include <cstdlib>
+#include <string>
+#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <limits>
+
 #define MURRAYC_DP_DEBUG_OUTPUT = 1;
 
 template <typename T>
@@ -109,6 +116,7 @@ class circular_vector {
 template <unsigned int T_COUNT_COSTS_TO_KEEP, typename T_cost>
 class DpBase {
 public:
+  using type_cost = T_cost;
   using type_costs = std::vector<T_cost>;
 
   /**
@@ -121,7 +129,7 @@ public:
     j_count_(j_count)
   {}
   
-  T_cost calc() {
+  type_cost calc() {
     for (unsigned int i = 0; i < i_count_; ++i) {
       type_costs& costs_i = costs_.get(0);
 
@@ -136,7 +144,7 @@ public:
         if (j != 0) {
           std::cout << ", ";
         }
-        std::cout << std::setw(2) << costs_i[j];
+        std::cout << std::setw(2) << costs_i[j].cost;
 #endif
       }
 
@@ -159,7 +167,7 @@ public:
   }
 
 private:
-  virtual T_cost calc_cost(unsigned int i, unsigned int j) const = 0;
+  virtual type_cost calc_cost(unsigned int i, unsigned int j) const = 0;
 
   /** Get the cell whose value contains the solution.
    */
@@ -172,8 +180,58 @@ protected:
   uint j_count_;
 };
 
+class Cost {
+public:
+  using uint = unsigned int;
+  
+  enum class Operation {
+    INVALID,
+    MATCH,
+    INSERT,
+    DELETE
+  };
+
+  using type_path = std::vector<Operation>;
+
+  explicit Cost()
+  : cost(0)
+  {}
+
+  Cost(uint in_cost, Operation initial_path)
+  : cost(in_cost),
+    path(1, initial_path)
+  {}
+  
+  Cost(uint in_cost, const type_path& in_path)
+  : cost(in_cost),
+    path(in_path)
+  {}
+
+  Cost(const Cost& src) = default;
+  Cost& operator=(const Cost& src) = default;
+
+  Cost(Cost&& src) = default;
+  Cost& operator=(Cost&& src) = default;
+
+  static std::string get_operation_as_string(Operation op) {
+    switch (op) {
+      case Operation::MATCH:
+        return "match";
+      case Operation::INSERT:
+        return "insert";
+      case Operation::DELETE:
+        return "delete";
+      default:
+        return "invalid";
+    }
+  }
+
+  uint cost;
+  type_path path;
+};
+
 class DpSubstringMatching
-  : public DpBase<2 /* cost to keep, used in calc_cost() */, uint> {
+  : public DpBase<2 /* cost to keep, used in calc_cost() */, Cost> {
 public:
   DpSubstringMatching(const std::string& str, const std::string& pattern)
   : DpBase(str.size() + 1, pattern.size() + 1),
@@ -182,28 +240,47 @@ public:
   {}
 
 private:
-  uint calc_cost(uint i, uint j) const override {
+  type_cost calc_cost(uint i, uint j) const override {
     if (i == 0) {
       //Base case:
-      return 0;
+      return Cost(0, Cost::Operation::INVALID);
     }
 
     if (j == 0) {
       //Base case:
-      return i * indel(' ');
+      return Cost(i * indel(' '), Cost::Operation::DELETE);
     }
 
     const type_costs& costs_i = costs_.get(0);
     const type_costs& costs_i_minus_1 = costs_.get(-1);
 
     //Get the cost of the possible operations, and choose the least costly:
-    const uint cost_match = costs_i_minus_1[j - 1] + match(str_[i], pattern_[j]);
-    const uint cost_insert = costs_i[j - 1] + indel(pattern_[j]);
-    const uint cost_delete = costs_i_minus_1[j] + indel(str_[j]);
+    const auto char_str_i = str_[i - 1]; //i is 1-indexed, but the str is 0-indexed.
+    const auto char_pattern_j = pattern_[j - 1]; //j is 1-indexed, but the pattern is 0-indexed.
+
+    const uint cost_match = costs_i_minus_1[j - 1].cost + match(char_str_i, char_pattern_j);
+    const uint cost_insert = costs_i[j - 1].cost + indel(char_pattern_j);
+    const uint cost_delete = costs_i_minus_1[j].cost + indel(char_str_i);
     
     auto min = std::min(cost_match, cost_insert);
     min = std::min(min, cost_delete);
-    return min;
+    
+    //Remember the path, based on what operation produced this minimum cost:
+    Cost::type_path path;
+    if (min == cost_match) {
+        path = costs_i_minus_1[j - 1].path;
+        path.emplace_back(Cost::Operation::MATCH);
+    } else if (min == cost_insert) {
+        path = costs_i[j - 1].path;
+        path.emplace_back(Cost::Operation::INSERT);
+    } else if (min == cost_delete) {
+        path = costs_i_minus_1[j].path;
+        path.emplace_back(Cost::Operation::DELETE);
+    } else {
+      std::cerr << "Unexpected min." << std::endl;
+    }
+
+    return Cost(min, path);
   }
 
   void get_goal_cell(unsigned int& i, unsigned int& j) const override {
@@ -215,7 +292,7 @@ private:
      uint min_cost = std::numeric_limits<uint>::max();
      unsigned int j_min_cost = 0;
      for (unsigned int k = 1; k < j_count_; ++k) {
-       const auto& cost = costs_i[k];
+       const auto& cost = costs_i[k].cost;
        if (cost < min_cost) {
           min_cost = cost;
           j_min_cost = k;
@@ -247,10 +324,16 @@ int main() {
   const auto pattern = "skiena";
   
   DpSubstringMatching dp(str, pattern);
-  const auto distance = dp.calc();
+  const auto result = dp.calc();
   std::cout << "string: " << str << std::endl
     << "pattern: " << pattern << std::endl
-    << "distance: " << distance << std::endl;
+    << "distance: " << result.cost << std::endl;
+
+  std::cout << "Operations: ";
+  for (const auto op : result.path) {
+    std::cout << Cost::get_operation_as_string(op) << ", ";
+  }
+  std::cout << std::endl;
   
   return EXIT_SUCCESS;
 }
